b0a42123390f3cfc4a08d2b1df3cbe404bb47fb4
Steve C <BuzzKanga@users.noreply.github.com>
2023-08-10 07:12:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMmeber() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'Yoon',
+        age: 26,
+        skills: skills
+    };
+    return member;
+}
